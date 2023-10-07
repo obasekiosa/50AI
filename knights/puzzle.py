1c7bd6ee498a22177dde712ec02c749df84422cc
Osakpolor Obaseki <obasekiosa@gmail.com>
2023-10-08 00:05:07 +0100

    Implement knights

--- knights/puzzle.py
+++ knights/puzzle.py
@@ -9,24 +9,36 @@ BKnave = Symbol("B is a Knave")
 CKnight = Symbol("C is a Knight")
 CKnave = Symbol("C is a Knave")
 
+def exclusive_or(a, b):
+    return Or(And(a, Not(b)), And(Not(a), b))
+
+def maybe_statement(sayer, nay_sayer, statement):
+    return Or(And(sayer, statement), And(nay_sayer, Not(statement)))
+
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    exclusive_or(AKnight, AKnave),
+    maybe_statement(AKnight, AKnave, And(AKnight, AKnave))
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    exclusive_or(AKnight, AKnave),
+    exclusive_or(BKnight, BKnave),
+    maybe_statement(AKnight, AKnave, And(AKnave, BKnave))
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    exclusive_or(AKnight, AKnave),
+    exclusive_or(BKnight, BKnave),
+    maybe_statement(AKnight, AKnave, Or(And(AKnight, BKnight), And(AKnave, BKnave))),
+    maybe_statement(BKnight, BKnave, Or(And(AKnight, BKnave), And(AKnave, BKnight)))
 )
 
 # Puzzle 3
@@ -35,7 +47,12 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    exclusive_or(AKnight, AKnave),
+    exclusive_or(BKnight, BKnave),
+    exclusive_or(CKnight, CKnave),
+    maybe_statement(AKnight, AKnave, Or(AKnight, AKnave)),
+    maybe_statement(BKnight, BKnave, CKnave),
+    maybe_statement(CKnight, CKnave, AKnight)
 )
 
 
